8a152f53a380c403663f206a14f31d66a2655d1c
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-31 17:56:50 +0200

    Bonus (AutoPlay)

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,45 +1,63 @@
-var app = new Vue(
-    {
-        el: '#root',
+var app = new Vue({
+    el: '#root',
 
-        data:{
-            bg_circle: 'bg-white',
-            // Array delle immagini
-            arrayImg: [
-                './asset/img/desert.jpg',
-                './asset/img/mountain.jpg',
-                './asset/img/tree_alone.jpg',
-                './asset/img/trees.jpg'
-            ],
-            
-            // Contatore per scorrere le immagini
-            counter: 0
-        },
-        methods:{
-            // Funzione per scrollare le foto verso sinistra
-            scrollLeft: function(){
-                // Se uguale a zero riparto dall'ultima foto
-                if(this.counter == 0){
-                    return this.counter = this.arrayImg.length - 1;
-                }else{
-                    return this.counter--
-                }
-            },
-            // Funzione per scrollare le foto verso destra
-            scrollRight: function(){
-                // Se uguale all'ultimo riparto dalla foto iniziale
-                if(this.counter == this.arrayImg.length - 1){
-                     this.counter = 0;
-                     console.log(this.counter)
-                }else{
-                     this.counter++
-                     console.log(this.counter)
-                }
-            },
-            // Funzione per cliccare i pallini e cambiare immagine
-            clickCircle: function(i){
-                this.counter = i;
+    data: {
+        // Array delle immagini
+        arrayImg: [
+            './asset/img/desert.jpg',
+            './asset/img/mountain.jpg',
+            './asset/img/tree_alone.jpg',
+            './asset/img/trees.jpg'
+        ],
+
+        // Contatore per scorrere le immagini
+        counter: 0
+    },
+    // created(){
+    //     setInterval(this.autoPlay, 1000)
+    // },
+    methods: {
+
+        // Funzione per scrollare le foto verso sinistra
+        scrollLeft: function () {
+            // Se uguale a zero riparto dall'ultima foto
+            if (this.counter == 0) {
+                return this.counter = this.arrayImg.length - 1;
+            } else {
+                return this.counter--
             }
+        },
+
+        // Funzione per scrollare le foto verso destra
+        scrollRight: function () {
+            // Se uguale all'ultimo riparto dalla foto iniziale
+            if (this.counter == this.arrayImg.length - 1) {
+                this.counter = 0;
+            } else {
+                this.counter++
+            }
+        },
+
+        // Funzione per cliccare i pallini e cambiare immagine
+        clickCircle: function (i) {
+            this.counter = i;
+        },
+
+        //Funzione auto play
+        autoPlay: function () {
+            if (this.counter == 3) {
+                this.counter = 0;
+            } else {
+                this.counter++;
+            }
+            console.log(this.counter)
+        },
+
+        generaAutoPlay: function(){
+            setInterval(this.autoPlay, 3000)
         }
+
     }
-)
+})
+
+// console.log(setInterval(3, 1000))
