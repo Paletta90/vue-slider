293dcb7b6b270e73c156fbe0d999ab2896462311
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-31 16:25:06 +0200

    Utilizzo v-for

--- asset/script/main.js
+++ asset/script/main.js
@@ -3,6 +3,7 @@ var app = new Vue(
         el: '#root',
 
         data:{
+            bg_circle: 'bg-white',
             // Array delle immagini
             arrayImg: [
                 './asset/img/desert.jpg',
@@ -10,26 +11,37 @@ var app = new Vue(
                 './asset/img/tree_alone.jpg',
                 './asset/img/trees.jpg'
             ],
+            
             // Contatore per scorrere le immagini
             counter: 0
         },
         methods:{
+            // Funzione per scrollare le foto verso sinistra
             scrollLeft: function(){
                 // Se uguale a zero riparto dall'ultima foto
                 if(this.counter == 0){
-                    return this.counter = this.arrayImg.lenght - 1;
+                    return this.counter = this.arrayImg.length - 1;
                 }else{
                     return this.counter--
                 }
             },
+            // Funzione per scrollare le foto verso destra
             scrollRight: function(){
                 // Se uguale all'ultimo riparto dalla foto iniziale
-                if(this.counter == this.arrayImg.lenght - 1){
+                if(this.counter == this.arrayImg.length - 1){
                      this.counter = 0;
                      console.log(this.counter)
                 }else{
                      this.counter++
                      console.log(this.counter)
+                }
+            },
+            // Funzione per cliccare i pallini e cambiare immagine
+            clickCircle: function(){
+                if(this.bg_circle == 'bg-white'){
+                    this.bg_circle = 'bg-primary'
+                }else{
+                    this.bg_circle = 'bg-white'
                 }
             }
         }
